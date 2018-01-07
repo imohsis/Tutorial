783048c2e1af75401fb0a7e2acf99435e87b5c30
imoh <imohsis@gmail.com>
2018-01-07 04:56:19 +0000

    changed the way my error is loged

--- config/app.php
+++ config/app.php
@@ -121,9 +121,11 @@ return [
     |
     */
 
-    'log' => env('APP_LOG', 'single'),
+      'log' => 'errorlog',
 
-    'log_level' => env('APP_LOG_LEVEL', 'debug'),
+//    'log' => env('APP_LOG', 'single'),
+//
+//    'log_level' => env('APP_LOG_LEVEL', 'debug'),
 
     /*
     |--------------------------------------------------------------------------
