7a9684cb97a4f426bacb940f89dc301aab5367f5
imoh <imohsis@gmail.com>
2018-04-16 10:53:09 +0100

    i cant believe i didnot commit this

--- config/database.php
+++ config/database.php
@@ -1,14 +1,5 @@
 <?php
 
-$url = parse_url(getenv("mysql://b749d997dd66bd:5c63d435@us-cdbr-iron-east-05.clear
-db.net/heroku_9e29c538261fb43?reconnect=true"));
-
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
-
-
 return [
 
     /*
@@ -50,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', $host),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', $database),
-            'username' => env('DB_USERNAME', $username),
-            'password' => env('DB_PASSWORD', $password),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
